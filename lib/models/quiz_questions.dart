5532ec11c0d4f4f4587ab2bcd9636c52450d874a
Nelly Tobías <nelly_anahi_17@hotmail.com>
2023-07-07 14:12:31 -0600

    Mutating Values in Memory

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -1,5 +1,14 @@
 class QuizQuestion {
   const QuizQuestion(this.text, this.answers);
+
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    //Crea ua lista basada en la lista answers.
+    final shuffledList = List.of(answers);
+    //Barajea la lista shuffledList sin cambiar los datos de la lista original.
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
