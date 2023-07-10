fe939de53868779799fa4d6dc75350061d85a70e
Nelly Tobías <nelly_anahi_17@hotmail.com>
2023-07-10 13:27:04 -0600

    Passing Data to the Results Screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,5 @@
 import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
-
 import 'package:adv_basics/start_screen.dart';
 import 'package:adv_basics/questions_screen.dart';
 import 'package:adv_basics/results_screen.dart';
@@ -45,7 +44,7 @@ class _QuizState extends State<Quiz> {
     }
 
     if (activeScreen == 'results-screen') {
-      screenWidget = const ResultsScreen();
+      screenWidget = ResultsScreen(chooseAnswers: selectedAnswers);
     }
 
     return MaterialApp(
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({super.key, required this.chooseAnswers});
+  final List<String> chooseAnswers;
 
   @override
   Widget build(BuildContext context) {
